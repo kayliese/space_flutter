e7a0351a27efd01282898278674f3b299d2acd04
Kayliese Smith <kas547@pitt.edu>
2022-11-06 14:11:50 -0800

    added bottom navigation

---
+++ my_app/lib/pages/navpages/launchpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+
+class LaunchPage extends StatelessWidget {
+  const LaunchPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text("Launch Page"),
+    ));
+  }
+}
---
+++ my_app/lib/pages/navpages/main_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/pages/navpages/eventpage.dart';
+import 'package:my_app/pages/navpages/homepage.dart';
+import 'package:my_app/pages/navpages/launchpage.dart';
+import 'package:http/http.dart' as http;
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [HomePage(), EventPage(), LaunchPage()];
+  int currentIndex = 0;
+
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff191826),
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          unselectedFontSize: 0,
+          selectedFontSize: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color(0xff191826),
+          onTap: onTap,
+          currentIndex: currentIndex,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey.withOpacity(0.5),
+          showUnselectedLabels: false,
+          showSelectedLabels: false,
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(label: ("Home"), icon: Icon(Icons.home)),
+            BottomNavigationBarItem(
+                label: ("Event"), icon: Icon(Icons.newspaper)),
+            BottomNavigationBarItem(
+                label: ("Launch"), icon: Icon(Icons.rocket_launch)),
+          ]),
+    );
+  }
+}
